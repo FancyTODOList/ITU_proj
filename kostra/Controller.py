d15a45e81bdd39a56b7b778e69e1286a32307f08
vazapa <zapletalvaclav201@gmail.com>
2023-12-17 12:02:45 +0100

    .

--- kostra/Controller.py
+++ kostra/Controller.py
@@ -1,8 +1,6 @@
 from tkinter import Toplevel, Text, Button, END
 from Model import TaskModel
-from View import TaskView, CalendarView
-from googletrans import Translator
-import speech_recognition as sr
+from View import TaskView
 import datetime
 
 # Todo list controller
@@ -12,6 +10,7 @@ class TaskController:
     def __init__(self, model, view):                                     # initialization
         self.model = model
         self.view = view
+
         self.selected_date = datetime.date.today().strftime("%d. %m. %Y")
         # load tasks from JSON file
         self.load_tasks()
@@ -19,10 +18,6 @@ class TaskController:
         self.view.delete_button.config(command=self.delete_item)
         self.view.add_button.config(command=self.add_item)
         self.view.complete_button.config(command=self.complete_item)
-        self.view.calendar_button.config(command=self.open_calendar)
-        self.view.translator_button.config(command=self.translate)
-        self.view.micro_button.config(
-            command=self.micro)  # set commands for buttons
 
     # delete one task function
     def delete_item(self):
@@ -58,36 +53,6 @@ class TaskController:
         # add task to listbox and json file and clear entry
         self.view.my_list.insert(END, task_text)
         self.view.my_entry.delete(0, END)
-
-    def micro(self):
-        r = sr.Recognizer()
-        mic = sr.Microphone()
-
-        # From micro
-        with mic as source:
-            try:
-                # 10 seconds waiting
-                audio = r.listen(source, timeout=10)
-                try:
-                    # English
-                    text = r.recognize_google(audio, language="en-US")
-                    # print("You said: " + text)
-                except sr.UnknownValueError:
-                    try:
-                        # Cestina
-                        text = r.recognize_google(audio, language="cs-CZ")
-                        # print("Řekli jste: " + text)
-                    except sr.UnknownValueError:
-                        # print("Sorry, speech not recognized.")
-                        text = "Sorry, speech not recognized."
-            except sr.WaitTimeoutError:
-                text = "No speech detected"
-
-            self.view.my_entry.delete(0, END)  # Clean Entry
-            # Put the text
-            self.view.my_entry.insert(0, text)
-
-    # complete/uncomplete one task function
 
     def complete_item(self):
         selected_indices = self.view.my_list.curselection()
@@ -129,71 +94,9 @@ class TaskController:
                 self.view.my_list.delete(index)
                 self.view.my_list.insert(index, f"✅ {task_text}")
 
-    # open calendar function
-
-    def open_calendar(self):
-        calendar_controller = CalendarController(None, None)
-        # Get the date using the controller's get_date function
-        self.today = calendar_controller.get_date()
-        # Create a CalendarView instance with the controller and pass the date
-        calendar_view = CalendarView(
-            Toplevel(self.view.root), calendar_controller, self.today)
-        # Set the controller on the view
-        calendar_controller.view = calendar_view
-        self.view.root.wait_window(calendar_view.root)
-
-        # Retrieve the selected date after the window is closed
-        self.selected_date = calendar_view.get_selected_date()
-        self.load_tasks()
-
-    def translate(self):                     # translate function
-        selected_indices = self.view.my_list.curselection()
-        from_language = self.view.from_language.get()
-        to_language = self.view.to_language.get()
-        translator = Translator()
-        if (self.view.my_entry.get() != ""):
-            task_text = self.view.my_entry.get()
-            translation = translator.translate(
-                task_text, src=from_language, dest=to_language)
-            self.view.my_entry.delete(0, END)  # Delete the current text
-            # Insert the translated text
-            self.view.my_entry.insert(0, translation.text)
-            return
-        elif (len(selected_indices) != 0):
-            # Get the tasks for the selected date
-            filtered_tasks = [
-                task for task in self.model.tasks if task["date"] == self.selected_date]
-    
-            for index in selected_indices:
-                # Check if the index is within the bounds of the filtered tasks list
-                if 0 <= index < len(filtered_tasks):
-                    # Get the index of the task within the entire tasks list
-                    global_index = self.model.tasks.index(
-                        filtered_tasks[index])
-    
-                    task_text = self.model.tasks[global_index]["text"]
-                    translation = translator.translate(
-                        task_text, src=from_language, dest=to_language)
-                    self.view.my_list.delete(index)
-                    self.view.my_list.insert(index, translation.text)
-                    # Update the task text in the model
-                    self.model.tasks[global_index]["text"] = translation.text
-            # Save the tasks
-            self.model.save_tasks()
-
     # load tasks to listbox function
     def load_tasks(self):
         tasks = self.model.load_tasks()
         self.view.display_tasks(tasks, self.selected_date)
 
-# Calendar controller
 
-
-class CalendarController:
-    def __init__(self, model, view):    # initialization
-        self.model = model
-        self.view = view
-
-    def get_date(self):
-        today = datetime.date.today()   # get today's date
-        return today
--- kostra/View.py
+++ kostra/View.py
@@ -1,155 +1,68 @@
 from tkinter import *
 from tkinter.font import Font
-from tkcalendar import Calendar
-from datetime import datetime
-
+from tkmacosx import Button
 
 class TaskView:
     def __init__(self, root, model):
         self.root = root
         self.model = model
-        self.root['bg'] = '#DDBEAA'
+        self.root['bg'] = 'blue'
         self.root.title('ToDo List!')
-        window_width = 600
-        window_height = 600
+        window_width = 700
+        window_height = 500
 
+        header_color = "#4d5198"
+        body_color = "#daf2dc"
+        button_color = header_color
+        button_text_color = "white"
+
+        # Get screen dimensions
         screen_width = self.root.winfo_screenwidth()
         screen_height = self.root.winfo_screenheight()
 
+        # Calculate the position for centering
         x = (screen_width - window_width) // 2
         y = (screen_height - window_height) // 2
 
+        # Set the window geometry
         self.root.geometry(f"{window_width}x{window_height}+{x}+{y}")
-        self.root.resizable(width=False, height=False)
 
-        self.my_font = Font(family="Regular", size=20, weight="bold")
+        self.my_font = Font(family="Arial", size=20, weight="bold")
 
-        self.my_frame = Frame(root)
-        self.my_frame.pack(pady=10)
-
-        self.my_list = Listbox(self.my_frame, font=self.my_font, width=35, height=7, bg="#DDBEAA", bd=0,
-                               fg="#000000", highlightthickness=0, selectbackground="#a6a6a6", activestyle="none")
-        self.my_list.pack(side=LEFT, fill=BOTH)
-
-        self.my_scrollbar = Scrollbar(self.my_frame)
-        self.my_scrollbar.pack(side=RIGHT, fill=BOTH)
-
-        self.my_list.config(yscrollcommand=self.my_scrollbar.set)
-        self.my_scrollbar.config(command=self.my_list.yview)
-
-        self.my_entry = Entry(root, font=("Helvetica", 20))
-        self.my_entry.pack(pady=20)
-
-        self.button_frame = Frame(root, background="#DDBEAA")
-        self.button_frame.pack(pady=20)
-
-        self.delete_button = Button(
-            self.button_frame, text="Delete Task", command=self.delete_item, background="#469597")
-        self.add_button = Button(
-            self.button_frame, text="Add Task", command=self.add_item, background="#469597")
-        self.complete_button = Button(
-            self.button_frame, text="Complete", command=self.complete_item, background="#469597")
-        self.calendar_button = Button(
-            self.button_frame, text="Calendar", command=self.open_calendar, background="#469597")
-        self.translator_button = Button(
-            self.button_frame, text="Translate", command=self.translate, background="#469597")
-        self.micro_button = Button(
-            self.button_frame, text="Micro", command=self.translate, background="#469597")
-        self.delete_button.grid(
-            row=0, column=2, sticky='ew', pady=10, ipadx=10)
-        self.add_button.grid(row=1, column=0, columnspan=4,
-                             sticky='ew', pady=10)
-        self.complete_button.grid(
-            row=0, column=1, sticky='ew', pady=10, ipadx=10)
-        self.calendar_button.grid(
-            row=0, column=0, sticky='ew', pady=10, ipadx=10)
-        self.translator_button.grid(
-            row=0, column=3, sticky='ew', pady=10, ipadx=10)
-        self.micro_button.grid(
-            row=2, column=0, columnspan=4, sticky='ew', pady=10)
-
-        # List of languages
-        self.short_languages = ['English', 'Polish',
-                                'Czech', 'Slovak', 'Ukrainian', 'Bulgarian']
-        self.long_languages = ['English', 'Spanish', 'French', 'German', 'Italian', 'Chinese',
-                               'Japanese', 'Korean', 'Arabic', 'Hindi', 'Turkish', 'Portuguese',
-                               'Dutch', 'Polish', 'Romanian', 'Greek', 'Swedish', 'Czech', 'Danish',
-                               'Finnish', 'Hungarian', 'Norwegian', 'Slovak', 'Ukrainian', 'Bulgarian',
-                               'Croatian', 'Lithuanian', 'Slovenian', 'Estonian', 'Latvian', 'Maltese',
-                               'Afrikaans', 'Albanian', 'Amharic', 'Armenian', 'Azerbaijani', 'Basque',
-                               'Belarusian', 'Bengali', 'Bosnian', 'Catalan', 'Cebuano', 'Chichewa',
-                               'Corsican', 'Filipino', 'Frisian', 'Galician', 'Georgian', 'Gujarati',
-                               'Haitian Creole', 'Hausa', 'Hawaiian', 'Hmong', 'Icelandic', 'Igbo',
-                               'Indonesian', 'Irish', 'Javanese', 'Kannada', 'Kazakh', 'Khmer',
-                               'Kurdish', 'Kyrgyz', 'Lao', 'Latin',
-                               'Macedonian', 'Malagasy', 'Malay', 'Malayalam', 'Maori', 'Marathi',
-                               'Mongolian', 'Myanmar', 'Nepali', 'Pashto', 'Persian',
-                               'Punjabi', 'Samoan', 'Scots Gaelic', 'Serbian', 'Sesotho', 'Shona',
-                               'Sindhi', 'Sinhala', 'Somali', 'Spanish', 'Sundanese', 'Swahili',
-                               'Tajik', 'Tamil', 'Telugu', 'Thai', 'Uzbek', 'Vietnamese', 'Welsh',
-                               'Xhosa', 'Yiddish', 'Yoruba', 'Zulu']
-
-        # Create variables to hold the selected languages
-        self.from_language = StringVar(self.root)
-        self.to_language = StringVar(self.root)
-
-        # Set default languages
-        self.from_language.set('Czech')
-        self.to_language.set('English')
-
-        # Create dropdown lists
-        self.from_language_menu = OptionMenu(
-            self.root, self.from_language, *self.short_languages)
-        self.to_language_menu = OptionMenu(
-            self.root, self.to_language, *self.short_languages)
-
-        # Place the dropdown lists
-        self.from_language_menu.place(x=500, y=340)
-        self.from_language_menu.config(bg="#469597", highlightthickness=0)  
-        self.to_language_menu.place(x=500, y=380)
-        self.to_language_menu.config(bg="#469597", highlightthickness=0)
-        # Create a variable to hold the state of the checkmark
-        self.long_languages_var = BooleanVar()
-
-        # Associate the variable with the checkmark
-        self.long_languages_checkmark = Checkbutton(
-            self.root, text="I'm polyglot", variable=self.long_languages_var, command=self.update_language_list, background="#DDBEAA", highlightthickness=0)
-        self.long_languages_checkmark.place(x=500, y=420)
+        # Header Frame
+        self.header_frame = Frame(root, background=header_color, height=window_height // 2)
+        self.header_frame.grid(row=0, column=0, sticky="nsew")
         
-        # Create a label to display the time
-        self.time_label = Label(root, text="", font=("Helvetica", 24))
-        self.time_label.pack(pady=20)
+        # Header
+        self.header_label = Label(self.header_frame, text="ToDo list", font=("Arial", 50), bg= header_color,fg="white")
+        self.header_label.pack(expand=True, anchor="center")  # Use pack instead of grid
 
-        # Update the time label every second
-        self.update_time_label()
+        # Body Frame
+        self.body_frame = Frame(root, background=body_color, height=window_height // 2)
+        self.body_frame.grid(row=1, column=0, sticky="nsew")
 
-    def update_time_label(self):
-        # Get the current time
-        current_time = datetime.now().strftime("%H:%M:%S")
+        self.my_entry = Entry(self.body_frame, font=("Helvetica", 20))
+        self.my_entry.grid(row=0, column=0, sticky="nsew", pady=10, padx=10)
 
-        # Update the time label
-        self.time_label.config(text=f"{current_time}", background="#DDBEAA", foreground="#469597")
+        self.my_list = Listbox(self.body_frame, font=self.my_font, bd=0, fg="#000000", highlightthickness=0,background=body_color, activestyle="none")
+        self.my_list.grid(row=1, column=0, sticky="nsew", pady=10, padx=10)  # Set columnspan to make it span two columns
 
-        # Call this method again after 1000 milliseconds
-        self.root.after(1000, self.update_time_label)
-            
-    def update_language_list(self):
-        # Check if the checkmark is checked
-        if self.long_languages_var.get():
-            # If it's checked, use the long language list
-            languages = self.long_languages
-        else:
-            # If it's unchecked, use the short language list
-            languages = self.short_languages
+        # Buttons
+        self.add_button = Button(self.body_frame, text="Add Task", command=self.add_item,background=header_color,fg=button_text_color)
+        self.add_button.grid(row=0, column=1, sticky="nsew", pady=10, padx=10)
 
-        # Update the dropdown lists
-        self.from_language_menu['menu'].delete(0, 'end')
-        self.to_language_menu['menu'].delete(0, 'end')
-        for language in languages:
-            self.from_language_menu['menu'].add_command(
-                label=language, command=lambda l=language: self.from_language.set(l))
-            self.to_language_menu['menu'].add_command(
-                label=language, command=lambda l=language: self.to_language.set(l))
+
+        self.delete_button = Button(self.body_frame, text="❌", command=self.delete_item,background=button_color,fg=button_text_color)
+        self.delete_button.grid(row=1, column=1, sticky="nsew", pady=10, padx=10)
+
+        self.complete_button = Button(self.body_frame, text="Complete", command=self.complete_item,background=button_color,fg=button_text_color)
+        self.complete_button.grid(row=2, column=0, sticky="nsew", pady=10, padx=10, columnspan=2)  # Set columnspan to make it span two columns
+
+        # Make columns and rows expandable
+        self.root.grid_columnconfigure(0, weight=1)
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_rowconfigure(1, weight=1)
+        self.body_frame.grid_columnconfigure(0, weight=1)  # Make the body_frame column expandable
 
     def display_tasks(self, tasks, selected_date):
         self.my_list.delete(0, END)
@@ -167,46 +80,3 @@ class TaskView:
 
     def complete_item(self):
         pass
-
-    def open_calendar(self):
-        pass
-
-    def translate(self):
-        pass
-
-    def micro(self):
-        pass
-
-
-class CalendarView:
-    def __init__(self, root, controller, initial_date):
-        self.root = root
-        self.root.title('Calendar')
-        self.controller = controller
-        self.root.configure(background='#469597')
-        self.root.resizable(width=False, height=False)
-
-        window_width = 450
-        window_height = 350
-
-        screen_width = self.root.winfo_screenwidth()
-        screen_height = self.root.winfo_screenheight()
-
-        x = (screen_width - window_width) // 2
-        y = (screen_height - window_height) // 2
-
-        self.get_date_button = Button(
-            self.root, text="Get date", command=self.get_selected_date,  background="#5BA199")
-        self.get_date_button.place(x=180, y=200)
-
-        self.root.geometry(f"{window_width}x{window_height}+{x}+{y}")
-
-        self.cal = Calendar(self.root, selectmode="day",
-                            year=initial_date.year, month=initial_date.month, day=initial_date.day)
-        self.cal.pack(padx=10, pady=10)
-
-    def get_selected_date(self):
-        selected_date_str = self.cal.get_date()
-        selected_date = datetime.strptime(selected_date_str, "%m/%d/%y")
-        formatted_date = selected_date.strftime("%d. %m. %Y")
-        return formatted_date
